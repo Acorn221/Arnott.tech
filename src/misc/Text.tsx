c9105851657c990d209e02a654fc1f7d7c9e611f
Acorn221 <jamesarnott2@gmail.com>
2022-07-16 19:26:59 +0100

    feat: added text file to store all the text

---
+++ src/misc/Text.tsx
@@ -0,0 +1,12 @@
+const Text = {
+  home: {
+    title: "James' Website",
+    content:
+      "Hi, I'm a Computer Science Student / Web Developer "
+      + "from Royal Holloway, University Of London. I'm currently"
+      + "in my final year of studies, and I'm currently working on"
+      + 'lots of projects, here are a few of my completed ones',
+  },
+};
+
+export default Text;
