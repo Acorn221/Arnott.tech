889185f453f22185260fbece1b94fc15a381f7d1
Acorn221 <jamesarnott2@gmail.com>
2022-07-16 19:29:26 +0100

    feat: added front page content

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,27 @@
+import Text from '@/misc/Text';
+import Carosel from './Carosel';
+import Slide from './Carosel/Slide';
+
+const txt = Text.home;
+
 const Home = () => (
-  <div>
-    <h1>Home</h1>
+  <div className="bg-slate-900">
+    <Carosel className="h-[25vh]">
+      <Slide className="">
+        <div>hi</div>
+      </Slide>
+      <Slide className="bg-white">
+        <div>hi</div>
+      </Slide>
+    </Carosel>
+    <div className=" h-[75vh]">
+      <div className="w-screen h-screen grid grid-cols-2 align-middle justify-center ">
+        <div className="text-2xl text-center text-white bg-slate-700 m-auto p-5 rounded-xl max-w-[45vw]">{txt.content}</div>
+        <div className="text-5xl text-center m-auto text-white">{txt.title}</div>
+      </div>
+    </div>
   </div>
+
 );
 
 export default Home;
