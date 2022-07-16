3cf30fadea92eb1c349f73fa20d8cd8f45f07039
Acorn221 <jamesarnott2@gmail.com>
2022-07-16 21:55:48 +0100

    feat: created initial frontend slide

---
+++ src/pages/Home/Carosel/Slides/Frontend/index.tsx
@@ -0,0 +1,9 @@
+import Slide from '../Slide';
+
+const Frontend = () => (
+  <Slide className="bg-gradient-to-r from-yellow-700 to-red-700 text-white text-3xl">
+    <div>hi</div>
+  </Slide>
+);
+
+export default Frontend;
