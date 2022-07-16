3dfd079109d42c759c815bb2aa3bcc11e444c910
Acorn221 <jamesarnott2@gmail.com>
2022-07-16 21:54:03 +0100

    chore: updating text

--- src/misc/Text.tsx
+++ src/misc/Text.tsx
@@ -1,11 +1,11 @@
 const Text = {
   home: {
-    title: "James' Website",
+    title: 'James Arnott',
     content:
       "Hi, I'm a Computer Science Student / Web Developer "
-      + "from Royal Holloway, University Of London. I'm currently"
-      + "in my final year of studies, and I'm currently working on"
-      + 'lots of projects, here are a few of my completed ones',
+      + "from Royal Holloway, University of London. I'm currently "
+      + 'in my final year of studies there. I like doing full stack '
+      + 'web development and doing web-based projects mainly.',
   },
 };
 
