60308024d1a8426ba3b182f396cc83f23d5fa25a
Acorn221 <jamesarnott2@gmail.com>
2022-07-16 16:26:33 +0100

    chore: init repo

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import Home from '@/pages/Home';
+import NotFound from '@/pages/NotFound';
+import StandardLayout from '@/layout/StandardLayout';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route element={<StandardLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  </Router>
+);
+
+export default App;
---
+++ src/layout/StandardLayout.tsx
@@ -0,0 +1,9 @@
+import { Outlet } from 'react-router';
+
+const StandardLayout = () => (
+  <div>
+    <Outlet />
+  </div>
+);
+
+export default StandardLayout;
