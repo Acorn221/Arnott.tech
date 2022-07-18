0f19aefb797309d31c6cb0bde915ef81ca308ac1
James Arnott <jamesarnott2@gmail.com>
2022-07-18 18:07:12 +0100

    refactor: moved tools folder

---
+++ src/pages/Home/Carosel/Slides/Tools/index.tsx
@@ -0,0 +1,16 @@
+import Slide from '../Slide';
+
+const Tools = () => (
+  <Slide className="bg-gradient-to-r from-yellow-700 to-red-700 text-white text-3xl">
+    <div className="flex w-full h-full">
+      <div className="flex-1 h-full flex justify-center align-middle">
+        hi
+      </div>
+      <div className="flex-1 h-full flex justify-center align-middle">
+        hi
+      </div>
+    </div>
+  </Slide>
+);
+
+export default Tools;
