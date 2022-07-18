20f1e8d368ffe895e8297e7176661bfaaafb31a9
James Arnott <jamesarnott2@gmail.com>
2022-07-18 18:07:30 +0100

    feat: added Tools to the home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import Text from '@/misc/Text';
 import Carosel from './Carosel';
 import Frontend from './Carosel/Slides/Frontend';
+import Tools from './Carosel/Slides/Tools';
 
 const txt = Text.home;
 
@@ -9,7 +10,7 @@ const Home = () => (
     <div className="text-5xl text-center py-5 text-white h-[10vh] min-h-[2em] min-w-fit">{txt.title}</div>
     <Carosel className="h-[35vh] min-h-[5em]">
       <Frontend />
-      <Frontend />
+      <Tools />
     </Carosel>
     <div className="min-h-[5em] grid 2xl:grid-cols-3 md:grid-cols-2 grid-cols-1 align-middle justify-center gap-4 p-5">
       <div className="text-2xl text-center text-white bg-slate-700 p-5 rounded-xl md:max-w-[45vw] row-span-2">{txt.content}</div>
