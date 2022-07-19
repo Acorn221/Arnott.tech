85ff94a6371ac907af3287954a063735c53080b2
James Arnott <jamesarnott2@gmail.com>
2022-07-20 00:57:49 +0100

    chore: updated text

--- src/misc/Text.tsx
+++ src/misc/Text.tsx
@@ -1,14 +1,19 @@
 const Text = {
   home: {
     title: 'James Arnott',
-    content:
+    boxes: [
       "Hi, I'm a Computer Science Student / Web Developer "
       + "from Royal Holloway, University of London. I'm currently "
       + 'in my final year of studies there. I like doing full stack '
       + 'web development and doing web-based projects mainly.',
+      'This website is currently in development',
+    ],
     slides: {
       frontend: {
-        text: 'The Frontend Technologies I Work With',
+        text: 'The Frontend Web Technologies I\'m Familiar With',
+      },
+      backend: {
+        text: 'The Backend Web Technologies I\'m Familiar With',
       },
     },
   },
