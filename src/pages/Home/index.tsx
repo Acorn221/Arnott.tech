7f545fb32fc699f3b4292ba1a485538b8cd7de08
James Arnott <jamesarnott2@gmail.com>
2022-07-20 00:58:31 +0100

    feat: made the text boxes programatically generate from text

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,10 +3,11 @@ import Text from '@/misc/Text';
 import Carosel from './Carosel';
 import Frontend from './Carosel/Slides/Frontend';
 import Tools from './Carosel/Slides/Tools';
+import Backend from './Carosel/Slides/Backend';
 
 const txt = Text.home;
 
-const fadeAnimation = 'fade in-out delay-4 duration-4';
+const fadeAnimation = 'fade in-out delay-4 duration-24';
 
 const Home = () => (
   <div className="h-full">
@@ -19,15 +20,20 @@ const Home = () => (
       <div>
         <Carosel className="h-[35vh] min-h-[5em]">
           <Frontend />
-          <Tools />
+          <Backend />
+          {/* <Tools /> */}
         </Carosel>
       </div>
     </XyzTransition>
     <XyzTransition appear xyz={`${fadeAnimation} down-2 short-100%`}>
       <div className="min-h-[5em] grid 2xl:grid-cols-3 md:grid-cols-2 grid-cols-1 align-middle justify-center gap-4 p-5">
-        <div className="text-2xl text-center text-white bg-slate-700 p-5 rounded-xl md:max-w-[45vw] row-span-2">{txt.content}</div>
-        <div className="h-[50vh] text-2xl text-center text-white bg-slate-700 p-5 rounded-xl md:max-w-[45vw] order-2">{txt.content}</div>
-        <div className="h-[50vh] text-2xl text-center text-white bg-slate-700 p-5 rounded-xl md:max-w-[45vw] order-2">{txt.content}</div>
+        {
+          txt.boxes.map((box) => (
+            <div key={Math.random()} className="text-2xl text-center text-white bg-slate-700 p-5 rounded-xl md:max-w-[45vw] order-2">
+              {box}
+            </div>
+          ))
+        }
       </div>
     </XyzTransition>
   </div>
