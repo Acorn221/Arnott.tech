df1c53547ddba8ecb286502a8f6e1b64b6a80f66
James <jamesarnott2@gmail.com>
2022-07-21 16:33:56 +0100

    feat: added context for the slides to know if they're being shown

--- src/pages/Home/Carosel/index.tsx
+++ src/pages/Home/Carosel/index.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, createContext } from 'react';
 import { useKeenSlider } from 'keen-slider/react';
 import './carosel.css';
 import 'keen-slider/keen-slider.min.css';
 import Arrow from './Arrow';
+
+const CaroselContext = createContext(0);
 
 export interface CaroselProps {
   children: React.ReactNode;
@@ -48,41 +50,46 @@ const Carosel = ({ children, className, slideContainerClass }: CaroselProps) => 
   }, [lastChanged]);
 
   return (
+
     <div className={className}>
       <div className="navigation-wrapper shadow-2xl">
-        <div ref={sliderRef} className={`keen-slider w-full h-[30vh] min-h-[10em] ${slideContainerClass}`} onKeyUp={handleKeyUp} tabIndex={0}>
-          {children}
-        </div>
+        <CaroselContext.Provider value={currentSlide}>
+          <div ref={sliderRef} className={`keen-slider w-full h-[30vh] min-h-[10em] ${slideContainerClass}`} onKeyUp={handleKeyUp} tabIndex={0}>
+            {children}
+          </div>
+        </CaroselContext.Provider>
         {loaded && instanceRef.current && (
-        <>
-          <Arrow
-            left
-            onClick={(e: any) => e.stopPropagation() || instanceRef.current?.prev()}
-          />
-          <Arrow
-            onClick={(e: any) => e.stopPropagation() || instanceRef.current?.next()}
-          />
-        </>
+          <>
+            <Arrow
+              left
+              onClick={(e: any) => e.stopPropagation() || instanceRef.current?.prev()}
+            />
+            <Arrow
+              onClick={(e: any) => e.stopPropagation() || instanceRef.current?.next()}
+            />
+          </>
         )}
       </div>
       {loaded && instanceRef.current && (
-      <div className="dots">
-        {[
-          ...Array(instanceRef.current.track.details.slides.length).keys(),
-        ].map((idx) => (
+        <div className="dots">
+          {[
+            ...Array(instanceRef.current.track.details.slides.length).keys(),
+          ].map((idx) => (
           // eslint-disable-next-line jsx-a11y/control-has-associated-label
-          <button
-            key={idx}
-            onClick={() => {
-              instanceRef.current?.moveToIdx(idx);
-            }}
-            className={`dot${currentSlide === idx ? ' active' : ''}`}
-          />
-        ))}
-      </div>
+            <button
+              key={idx}
+              onClick={() => {
+                instanceRef.current?.moveToIdx(idx);
+              }}
+              className={`dot${currentSlide === idx ? ' active' : ''}`}
+            />
+          ))}
+        </div>
       )}
     </div>
   );
 };
 
 export default Carosel;
+
+export { CaroselContext };
