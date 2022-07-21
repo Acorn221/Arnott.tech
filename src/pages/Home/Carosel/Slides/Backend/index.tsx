a0d34eedf2be57c0b1071a9b61227c538e2bd524
James <jamesarnott2@gmail.com>
2022-07-21 16:34:10 +0100

    feat: added animation play on slide show

--- src/pages/Home/Carosel/Slides/Backend/index.tsx
+++ src/pages/Home/Carosel/Slides/Backend/index.tsx
@@ -10,8 +10,11 @@ import PythonIcon from 'devicon/icons/python/python-original.svg';
 import MySQLIcon from 'devicon/icons/mysql/mysql-original.svg';
 import NextJSIcon from 'devicon/icons/nextjs/nextjs-line.svg';
 import AWSIcon from 'devicon/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg';
+import { useContext, useEffect, useState } from 'react';
 import LambdaIcon from './assets/lambda.svg';
 import DynamoDBIcon from './assets/dynamoDB.svg';
+
+import { CaroselContext } from '@/pages/Home/Carosel/';
 
 import Text from '@/misc/Text';
 import Slide from '../Slide';
@@ -20,31 +23,49 @@ const txt = Text.home.slides.backend;
 
 const iconStyles = 'h-[12vmin] max-h-[8vh]';
 
-const Backend = () => (
-  <Slide className="bg-gradient-to-r from-yellow-700 to-red-700 text-white text-3xl">
-    <div className="flex w-full h-full md:flex-row flex-col-reverse">
-      <div className="flex-auto h-full flex justify-center align-middle">
-        <XyzTransitionGroup appear xyz="fade flip-up flip-left delay-5 stagger" className="m-auto brightness-0 invert grid grid-cols-4 gap-2 md:p-10">
-          <img src={TypeScriptIcon} className={iconStyles} />
-          <img src={JavascriptIcon} className={iconStyles} />
-          <img src={GraphQLIcon} className={iconStyles} />
-          <img src={PostgresIcon} className={iconStyles} />
-          <img src={PythonIcon} className={iconStyles} />
-          <img src={FlaskIcon} className={iconStyles} />
-          <img src={GoIcon} className={iconStyles} />
-          <img src={MySQLIcon} className={iconStyles} />
-          <img src={NextJSIcon} className={iconStyles} />
-          <img src={AWSIcon} className={iconStyles} />
-          <img src={DynamoDBIcon} className={iconStyles} />
-          <img src={LambdaIcon} className={iconStyles} />
-        </XyzTransitionGroup>
-      </div>
+interface BackendInterface {
+  index: number;
+}
 
-      <div className="md:flex-1 flex-none align-middle m-auto">
-        <div className="p-3 mr-6 md:mr-12 text-xl md:text-3xl">{txt.text}</div>
+const Backend = ({ index }: BackendInterface) => {
+  const currentSlide = useContext(CaroselContext);
+  const [seen, setSeen] = useState(false);
+  useEffect(() => {
+    if (!seen) {
+      if (currentSlide === index) {
+        setSeen(true);
+      }
+    }
+  }, [currentSlide]);
+
+  return (
+    <Slide className="bg-gradient-to-r from-yellow-700 to-red-700 text-white text-3xl">
+      <div className="flex w-full h-full md:flex-row flex-col-reverse">
+        <div className="flex-auto h-full flex justify-center align-middle">
+          {seen && (
+          <XyzTransitionGroup appear xyz="fade flip-up flip-left delay-5 stagger" className="m-auto brightness-0 invert grid grid-cols-4 gap-2 md:p-10">
+            <img src={TypeScriptIcon} className={iconStyles} />
+            <img src={JavascriptIcon} className={iconStyles} />
+            <img src={GraphQLIcon} className={iconStyles} />
+            <img src={PostgresIcon} className={iconStyles} />
+            <img src={PythonIcon} className={iconStyles} />
+            <img src={FlaskIcon} className={iconStyles} />
+            <img src={GoIcon} className={iconStyles} />
+            <img src={MySQLIcon} className={iconStyles} />
+            <img src={NextJSIcon} className={iconStyles} />
+            <img src={AWSIcon} className={iconStyles} />
+            <img src={DynamoDBIcon} className={iconStyles} />
+            <img src={LambdaIcon} className={iconStyles} />
+          </XyzTransitionGroup>
+          )}
+        </div>
+
+        <div className="md:flex-1 flex-none align-middle m-auto">
+          <div className="p-3 mr-6 md:mr-12 text-xl md:text-3xl">{txt.text}</div>
+        </div>
       </div>
-    </div>
-  </Slide>
-);
+    </Slide>
+  );
+};
 
 export default Backend;
--- src/pages/Home/Carosel/Slides/Frontend/index.tsx
+++ src/pages/Home/Carosel/Slides/Frontend/index.tsx
@@ -7,8 +7,8 @@ import JavascriptIcon from 'devicon/icons/javascript/javascript-plain.svg';
 import JestIcon from 'devicon/icons/jest/jest-plain.svg';
 import MaterialUiIcon from 'devicon/icons/materialui/materialui-original.svg';
 import ReduxIcon from 'devicon/icons/redux/redux-original.svg';
-
-import { MouseEventHandler } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { CaroselContext } from '@/pages/Home/Carosel/';
 import Text from '@/misc/Text';
 import Slide from '../Slide';
 
@@ -16,27 +16,44 @@ const txt = Text.home.slides.frontend;
 
 const iconStyles = 'h-[12vmin] max-h-[10vh]';
 
-const Frontend = () => (
-  <Slide className="bg-gradient-to-r from-yellow-700 to-red-700 text-white text-3xl">
-    <div className="flex w-full h-full md:flex-row flex-col-reverse">
-      <div className="flex-auto h-full flex justify-center align-middle">
-        <XyzTransitionGroup appear xyz="fade flip-up flip-left delay-5 stagger" className="m-auto brightness-0 invert grid grid-cols-4 gap-2 md:p-10">
-          <img src={TypeScriptIcon} className={iconStyles} />
-          <img src={JavascriptIcon} className={iconStyles} />
-          <img src={ReactIcon} className={iconStyles} />
-          <img src={TailwindCss} className={iconStyles} />
-          <img src={ReduxIcon} className={iconStyles} />
-          <img src={MaterialUiIcon} className={iconStyles} />
-          <img src={JestIcon} className={iconStyles} />
-        </XyzTransitionGroup>
-      </div>
+interface FrontendInterface {
+  index: number;
+}
 
-      <div className="md:flex-1 flex-none align-middle m-auto">
-        <div className="p-3 mr-6 md:mr-12 text-xl md:text-3xl">{txt.text}</div>
-      </div>
-    </div>
-  </Slide>
-);
+const Frontend = ({ index }: FrontendInterface) => {
+  const currentSlide = useContext(CaroselContext);
+  const [seen, setSeen] = useState(false);
+  useEffect(() => {
+    if (!seen) {
+      if (currentSlide === index) {
+        setSeen(true);
+      }
+    }
+  }, [currentSlide]);
 
+  return (
+    <Slide className="bg-gradient-to-r from-yellow-700 to-red-700 text-white text-3xl">
+      <div className="flex w-full h-full md:flex-row flex-col-reverse">
+        <div className="flex-auto h-full flex justify-center align-middle">
+          {seen && (
+          <XyzTransitionGroup appear xyz="fade flip-up flip-left delay-5 stagger" className="m-auto brightness-0 invert grid grid-cols-4 gap-2 md:p-10">
+            <img src={TypeScriptIcon} className={iconStyles} />
+            <img src={JavascriptIcon} className={iconStyles} />
+            <img src={ReactIcon} className={iconStyles} />
+            <img src={TailwindCss} className={iconStyles} />
+            <img src={ReduxIcon} className={iconStyles} />
+            <img src={MaterialUiIcon} className={iconStyles} />
+            <img src={JestIcon} className={iconStyles} />
+          </XyzTransitionGroup>
+          )}
+        </div>
+
+        <div className="md:flex-1 flex-none align-middle m-auto">
+          <div className="p-3 mr-6 md:mr-12 text-xl md:text-3xl">{txt.text}</div>
+        </div>
+      </div>
+    </Slide>
+  );
+};
 
 export default Frontend;
