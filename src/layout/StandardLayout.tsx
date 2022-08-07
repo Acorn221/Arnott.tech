09179460a8c9d176641fb76ab487aeefa4e1c93e
James Arnott <jamesarnott2@gmail.com>
2022-08-07 21:23:05 +0100

    feat: added copyright / info footer

--- src/layout/StandardLayout.tsx
+++ src/layout/StandardLayout.tsx
@@ -1,8 +1,18 @@
 import { Outlet } from 'react-router';
+import { AiFillHeart } from 'react-icons/ai';
+import Text from '@/misc/Text';
+
+const txt = Text.layout.footer;
 
 const StandardLayout = () => (
-  <div className="h-full">
-    <Outlet />
+  <div className="flex flex-col h-screen justify-between">
+    <div className="h-full">
+      <Outlet />
+    </div>
+    <footer className="text-center p-3 bg-gradient-to-r from-orange-400 to-red-400 text-white font-semibold z-10 bottom-0 relative w-full">
+      {txt.content}
+      <AiFillHeart className="fill-red-600 inline align-middle ml-2" />
+    </footer>
   </div>
 );
 
