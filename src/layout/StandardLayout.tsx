a59c821adc91f3e549bcbf0b14a7551bb41acaed
James Arnott <jamesarnott2@gmail.com>
2022-08-08 13:31:55 +0100

    fix: footer is always at the bottom now

--- src/layout/StandardLayout.tsx
+++ src/layout/StandardLayout.tsx
@@ -6,12 +6,14 @@ const txt = Text.layout.footer;
 
 const StandardLayout = () => (
   <div className="flex flex-col h-screen justify-between">
-    <div className="h-full">
+    <main>
       <Outlet />
-    </div>
-    <footer className="text-center p-3 bg-gradient-to-r from-orange-400 to-red-400 text-white font-semibold z-10 bottom-0 relative w-full">
-      {txt.content}
-      <AiFillHeart className="fill-red-600 inline align-middle ml-2" />
+    </main>
+    <footer className="sticky top-[100vh]">
+      <div className="text-center p-3 bg-gradient-to-r from-orange-400 to-red-400 text-white font-semibold z-10">
+        {txt.content}
+        <AiFillHeart className="fill-red-600 inline align-middle ml-2" />
+      </div>
     </footer>
   </div>
 );
