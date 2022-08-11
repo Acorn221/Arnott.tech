7891b94b880ef9e027272b0f60e3055747bc5b74
James Arnott <jamesarnott2@gmail.com>
2022-08-11 13:35:20 +0100

    feat: added an email getter method with a ceasar cypher to mitigate webscrapers

---
+++ src/pages/Home/util/misc.ts
@@ -0,0 +1,20 @@
+const email = 'qhtlz@q4h.br';
+
+const shift = -7;
+/**
+ * Here I'm doing a ceasar cypher to avoid webscrapers finding my email
+ * and sending me spam and putting my compsci degree to real use 😎😎😎
+ */
+const getEmail = () => String.fromCharCode(...email.split('').map((char) => {
+  const charCode = char.toLowerCase().charCodeAt(0);
+  // if it's a lowercase letter
+  if (charCode >= 97 && charCode <= 122) {
+    // shift it, and keep it in the range of lowercase letters
+    return ((((charCode - 96 + shift) % 26) + 26) % 26) + 96;
+  }
+  // if it's not a lowercase letter, just return the original char
+  return charCode;
+}));
+
+// eslint-disable-next-line import/prefer-default-export
+export { getEmail };
