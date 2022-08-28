7127711920c10820c7d7f61d6c661042a8d33da3
James <jamesarnott2@gmail.com>
2022-08-28 12:41:02 +0100

    fix: removed unneeded vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
-import * as dotenv from 'dotenv';
-
-dotenv.config({ path: `${__dirname}/.env` });
-
-const ASSET_URL = (process.env.ASSET_URL && process.env.APP_ENV === 'local') ? process.env.ASSET_URL : '/J4a-website/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,5 +14,4 @@ export default defineConfig({
   server: {
     host: true,
   },
-  base: `${ASSET_URL}`,
 });
