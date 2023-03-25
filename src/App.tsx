50d68a3bb85243cdec99017ac11db7c4f3e3d6a8
James <jamesarnott2@gmail.com>
2023-03-25 14:10:15 +0000

    feat: added short URL rick roll

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from '@/pages/Home';
 import NotFound from '@/pages/NotFound';
 import StandardLayout from '@/layout/StandardLayout';
+import RickRoll from '@/misc/RickRoll';
 
 const App = () => (
   <Router>
     <Routes>
+      <Route path="/r" element={<RickRoll />} />
       <Route element={<StandardLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="J4a-website/" element={<Home />} />
