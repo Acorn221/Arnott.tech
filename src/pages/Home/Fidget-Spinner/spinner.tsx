7cb60b3cc360ee5aabc43c3336a7c53fec8a10d1
Acorn221 <james@arnott.tech>
2024-12-30 19:46:11 +0000

    feat: xray mode works

--- src/pages/Home/Fidget-Spinner/spinner.tsx
+++ src/pages/Home/Fidget-Spinner/spinner.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable no-param-reassign */
-import { FC, useRef, useEffect } from 'react';
+import {
+  FC, useRef, useEffect, useState,
+} from 'react';
 import { useGLTF } from '@react-three/drei';
 import * as THREE from 'three';
 import { useFrame, GroupProps, ThreeEvent } from '@react-three/fiber';
@@ -10,18 +12,12 @@ interface FidgetSpinnerProps extends GroupProps {
 }
 
 const createMaterials = () => ({
-  // Amoungi + Text - PRESENT IN CURRENT MODEL
+  // Amoungi + Text
   '0.000000_0.000000_0.000000_0.000000_0.000000': new THREE.MeshPhysicalMaterial({
     color: new THREE.Color('#FFFFFF'),
-    // metalness: 0.9,
-    // roughness: 0.15,
-    // envMapIntensity: 1.0,
-    // clearcoat: 1.0,
-    // clearcoatRoughness: 0.1,
   }),
   // Bearing casing
   '0.647059_0.647059_0.647059_0.000000_0.000000': new THREE.MeshPhysicalMaterial({
-    // color: new THREE.Color('#FF0000'),
     color: new THREE.Color('#e8e8e8'),
     metalness: 1.0,
     roughness: 0.05,
@@ -29,21 +25,19 @@ const createMaterials = () => ({
     clearcoat: 1.0,
     clearcoatRoughness: 0.03,
   }),
-  // Bearing Seal - PRESENT IN CURRENT MODEL
+  // Bearing Seal
   '0.000000_0.000000_1.000000_0.000000_0.000000': new THREE.MeshPhysicalMaterial({
     color: new THREE.Color('#0000FF'),
-    // metalness: 0.7,
     roughness: 0.3,
     envMapIntensity: 0.8,
   }),
-  // Main body of the spinner - PRESENT IN CURRENT MODEL
+  // Main body of the spinner
   '1.000000_0.000000_0.000000_0.000000_0.000000': new THREE.MeshPhysicalMaterial({
     color: new THREE.Color('#FF0000'),
     metalness: 0.7,
     roughness: 0.3,
-    // envMapIntensity: 1.2,
-    // clearcoat: 0.8,
-    // clearcoatRoughness: 0.1,
+    clearcoat: 0.8,
+    clearcoatRoughness: 0.1,
   }),
 });
 
@@ -52,6 +46,7 @@ const Spinner: FC<FidgetSpinnerProps> = (props) => {
   const isDragging = useRef(false);
   const previousMousePosition = useRef({ x: 0, y: 0 });
   const materials = useRef(createMaterials());
+  const [isXray, setIsXray] = useState(false);
 
   const { scene } = useGLTF('/fidget-spinner.gltf');
 
@@ -61,21 +56,66 @@ const Spinner: FC<FidgetSpinnerProps> = (props) => {
     }
   }, []);
 
-  scene.traverse((object) => {
-    if (!(object instanceof THREE.Mesh)) return;
+  // Store initial material setup
+  useEffect(() => {
+    const customMaterials = createMaterials();
+    materials.current = customMaterials;
 
-    const materialName = object.material?.name;
-    // @ts-ignore yeet
-    const mappedMaterial = materials.current[materialName] as THREE.Material;
+    scene.traverse((object) => {
+      if (object instanceof THREE.Mesh) {
+        // Store the material name as a custom property
+        // @ts-ignore
+        object.userData.materialKey = object.material.name;
 
-    if (mappedMaterial) {
-      object.material = mappedMaterial;
-    }
+        // Apply our custom materials initially
+        const materialKey = object.material.name;
+        // @ts-ignore - materialKey is a custom property
+        if (customMaterials[materialKey]) {
+          // @ts-ignore - materialKey is a custom property
+          object.material = customMaterials[materialKey];
+          object.castShadow = true;
+          object.receiveShadow = true;
+        }
+      }
+    });
+  }, [scene]);
 
-    // Enable shadows
-    object.castShadow = true;
-    object.receiveShadow = true;
-  });
+  useEffect(() => {
+    console.log('isXray changed to:', isXray);
+    scene.traverse((object) => {
+      if (object instanceof THREE.Mesh) {
+        if (isXray) {
+          object.material = new THREE.MeshPhysicalMaterial({
+            wireframe: true,
+            color: new THREE.Color('#bdc5c3'),
+            transparent: true,
+            opacity: 0.7,
+          });
+        } else {
+          // Use the stored material key to restore the correct material
+          const { materialKey } = object.userData;
+          // @ts-ignore - materialKey is a custom property
+          const originalMaterial = materials.current[materialKey];
+          if (originalMaterial) {
+            object.material = originalMaterial;
+            object.castShadow = true;
+            object.receiveShadow = true;
+          }
+        }
+      }
+    });
+  }, [isXray]);
+
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      if (event.key === 'x') {
+        setIsXray((prev) => !prev);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+    return () => window.removeEventListener('keydown', handleKeyPress);
+  }, []);
 
   const rotationSpeed = useRef(0);
   const targetSpeed = useRef(30);
