197c821d47f2abf7d9b35fda6db24be08e43fe2a
James Arnott <james@rno.tt>
2025-06-14 14:42:39 +0100

    chore: updated status - leaving 🍐!

--- src/misc/Text.tsx
+++ src/misc/Text.tsx
@@ -68,7 +68,7 @@ const Text: textType = {
       title: 'About Me',
       text: "I'm a Computer Science Graduate and Software Developer "
         + "from Royal Holloway, University of London. I'm currently "
-        + 'working at PearAI (YC F24). I like doing full stack '
+        + 'working on my own projects. I like doing full stack '
         + 'web development and doing web-based projects mainly, along with a variety of machine learning based projects',
     },
     projects: {
